03217dfb9d672f1188493fe42050e655b6871b25
filipgerdina <99407737+filipgerdina@users.noreply.github.com>
2022-12-23 23:36:19 +0100

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.9xncqhn.mongodb.net/?retryWrites=true&w=majority`, ()=> {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.s4yoqpu.mongodb.net/?retryWrites=true&w=majority`, ()=> {
   console.log('connected to mongodb')
 })
